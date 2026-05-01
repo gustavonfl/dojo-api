0f4f479dcae8753bef7c7ae5c980f3230a271e66
Gustavo Leite <qustavoleite@gmail.com>
2026-05-01 01:01:03 -0300

    feat: adiciona entidade de matricla fatura

---
+++ src/main/java/dev/gustavo/dojo/model/FaturaMatricula.java
@@ -0,0 +1,88 @@
+package dev.gustavo.dojo.model;
+
+import dev.gustavo.dojo.model.enums.StatusFatura;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class FaturaMatricula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    private BigDecimal valor;
+
+    @Column(name = "data_pagamento")
+    private LocalDateTime dataPagamento;
+
+    @Column(name = "data_cancelamento")
+    private LocalDateTime dataCancelamento;
+
+    @Enumerated(EnumType.STRING)
+    private StatusFatura status = StatusFatura.ABERTA;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "matricula_modalidade_id")
+    private Matricula matricula;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDateTime dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public LocalDateTime getDataCancelamento() {
+        return dataCancelamento;
+    }
+
+    public void setDataCancelamento(LocalDateTime dataCancelamento) {
+        this.dataCancelamento = dataCancelamento;
+    }
+
+    public StatusFatura getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusFatura status) {
+        this.status = status;
+    }
+
+    public Matricula getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Matricula matricula) {
+        this.matricula = matricula;
+    }
+}
