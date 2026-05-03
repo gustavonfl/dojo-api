84ed7ba960dffab72c3b1ffaf7cdffed5e3a4654
Gustavo Leite <qustavoleite@gmail.com>
2026-05-02 23:44:34 -0300

    feat: adiciona query para verificar email

--- src/main/java/dev/gustavo/dojo/repository/AlunoRepository.java
+++ src/main/java/dev/gustavo/dojo/repository/AlunoRepository.java
@@ -4,4 +4,5 @@ import dev.gustavo.dojo.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    boolean existsByEmail(String email);
 }
