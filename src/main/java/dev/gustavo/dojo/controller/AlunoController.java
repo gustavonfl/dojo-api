0c6541034261c55ceb9934b4f53182b21c05f8d9
Gustavo Leite <qustavoleite@gmail.com>
2026-05-03 12:46:56 -0300

    feat: adiciona a camada controller da entidade aluno

---
+++ src/main/java/dev/gustavo/dojo/controller/AlunoController.java
@@ -0,0 +1,46 @@
+package dev.gustavo.dojo.controller;
+
+import dev.gustavo.dojo.dto.AlunoRequest;
+import dev.gustavo.dojo.dto.AlunoResponse;
+import dev.gustavo.dojo.service.AlunoService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService){
+        this.alunoService = alunoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AlunoResponse cadastrar(@RequestBody AlunoRequest AlunoRequest){
+       return alunoService.cadastrar(AlunoRequest);
+    }
+
+    @GetMapping
+    public Page<AlunoResponse> listar(Pageable pageable){
+        return alunoService.listar(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public AlunoResponse buscarPorId(@PathVariable Long id){
+        return alunoService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public AlunoResponse atualizar(@PathVariable Long id, @RequestBody AlunoRequest alunoRequest){
+        return alunoService.atualizar(id, alunoRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long id){
+         alunoService.excluir(id);
+    }
+}
