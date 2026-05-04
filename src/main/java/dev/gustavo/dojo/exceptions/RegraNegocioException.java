17232832bcff637187eb26ae2360e21c24cb9d81
Gustavo Leite <qustavoleite@gmail.com>
2026-05-03 21:27:14 -0300

    feat: adiciona exception RegraNegocioException para validações de regras de negócio

---
+++ src/main/java/dev/gustavo/dojo/exceptions/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package dev.gustavo.dojo.exceptions;
+
+public class RegraNegocioException extends RuntimeException{
+    public RegraNegocioException(String message){
+        super(message);
+    }
+}
